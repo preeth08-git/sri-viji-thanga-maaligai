e95b0c33bc7b801e2e163b98da88ca663b2a972d
PraveenKumar <preethamkatariya@gmail.com>
2026-03-24 19:58:05 +0530

    Fixed layout, footer, whatsapp button

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,39 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Home from './pages/Home'
-import About from './pages/About'
-import Collections from './pages/Collections'
-import Contact from './pages/Contact'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Collections from "./pages/Collections";
+import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-export default function App() {
+function App() {
   return (
     <BrowserRouter>
       <Navbar />
+
+      {/* 🔥 IMPORTANT WRAPPER */}
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
           <Route path="/collections" element={<Collections />} />
+          <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </main>
+
       <Footer />
+
+      {/* ✅ WhatsApp Button */}
+      <a
+        href="https://wa.me/919840686575"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="whatsapp-btn"
+      >
+        WhatsApp
+      </a>
     </BrowserRouter>
-  )
+  );
 }
+
+export default App;
